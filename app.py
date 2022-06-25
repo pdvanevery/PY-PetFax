a519ac285f6c77d3fd6fabf8a4ba72684f6dff27
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-06-25 11:58:11 -0700

    added code factory

--- app.py
+++ app.py
@@ -1,13 +1,2 @@
-#configuration
-from flask import Flask 
-app = Flask(__name__)
-
-#index route
-@app.route('/')
-def index():
-    return 'Hello, this is PetFax!'
-
-#pet route
-@app.route('/pets')
-def pets():
-    return 'These are our pets available for adoption!'
+from petfax import create_app
+app = create_app()
