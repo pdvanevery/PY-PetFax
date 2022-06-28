20fa9cc31a8ea7d54e5536c0279eb0e38831fff1
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-06-27 21:21:25 -0700

    fact route works, need to add content

--- petfax/fact.py
+++ petfax/fact.py
@@ -1,7 +1,9 @@
-from flask import ( Blueprint, render_template)
+from flask import (Blueprint, render_template) 
 
-bp = Blueprint('fact', __name__, url_prefix='/facts')
+bp = Blueprint('fact', __name__, url_prefix="/facts")
 
 @bp.route('/new')
 def new():
     return render_template('facts/new.html')
+
+
