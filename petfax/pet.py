b6400a8b3d79b9b682036f2d250a16e2088605d4
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-06-27 20:13:00 -0700

    show route works, need to add content to it

--- petfax/pet.py
+++ petfax/pet.py
@@ -9,3 +9,8 @@ bp = Blueprint('pet', __name__, url_prefix='/pets')
 @bp.route('/')
 def index():
     return render_template('index.html', pets=pets)
+    
+@bp.route('/show')
+def show():
+    return render_template('show.html', pets=pets)
+
