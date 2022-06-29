f12ad5b3541daadcac18046d0ae85a2b1b903b2b
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-06-28 17:11:20 -0700

    show page works for each dog, just need to add content

--- petfax/pet.py
+++ petfax/pet.py
@@ -10,7 +10,8 @@ bp = Blueprint('pet', __name__, url_prefix='/pets')
 def index():
     return render_template('index.html', pets=pets)
     
-@bp.route('/show')
-def show():
+@bp.route('/<int:id>')
+def show(id):
+    pet = pets[id - 1]
     return render_template('show.html', pets=pets)
 
